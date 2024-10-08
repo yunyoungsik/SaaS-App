a7c2cb78c328ce595e8998553f8210852b6c50d3
yunyoungsik <yunyoungsik91@gmail.com>
2024-10-08 23:16:45 +0900

    [20241008]1:23:19까지 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./sections/Features"
 import Header from "./sections/Header"
 import Hero from "./sections/Hero"
 
@@ -6,6 +7,7 @@ const App = () => {
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
     </main>
   )
 }
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,16 +1,44 @@
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link as LinkScroll } from 'react-scroll';
 
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5">
-    {title}
-  </LinkScroll>
-);
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5"
+    >
+      {title}
+    </LinkScroll>
+  );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        'fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4',
+        hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]'
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a href="#" className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
@@ -34,7 +62,7 @@ const Header = () => {
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
@@ -53,9 +81,21 @@ const Header = () => {
               </ul>
             </nav>
 
-            <div className='lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90'>
-              <img src="/images/bg-outlines.svg" width={960} height={380} alt="outline" className='relative z-2' />
-              <img src="/images/bg-outlines-fill.png" width={960} height={380} alt="outline" className='absolute inset-0 mix-blend-soft-light opacity-5' />
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
             </div>
           </div>
         </div>
