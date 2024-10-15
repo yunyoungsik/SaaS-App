6388c925bc46047fea0bfc8c0aae5b9d9fda9db4
yunyoungsik <yunyoungsik91@gmail.com>
2024-10-15 21:42:17 +0900

    [20241015]완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Download from "./sections/Download"
 import Faq from "./sections/Faq"
 import Features from "./sections/Features"
+import Footer from "./sections/Footer"
 import Header from "./sections/Header"
 import Hero from "./sections/Hero"
 import Pricing from "./sections/Pricing"
+import Testimonials from "./sections/Testimonials"
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
   )
 }
